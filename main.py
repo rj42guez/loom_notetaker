31a1c8922e717593743e72912cc3698cc0c1a7de
Raphael Justin C. Portuguez <62928512+rj42guez@users.noreply.github.com>
2022-03-18 10:39:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from kivy.uix.popup import Popup
 from kivy.properties import ObjectProperty, StringProperty
 from kivy.uix.widget import Widget 
 
+# from tkinter import *
+from tkinter import Tk
+from tkinter import filedialog
+from tkinter import messagebox
 import os
 
 class FileChoose(Popup):
@@ -15,14 +19,59 @@ class MyGridLayout(Widget):
     text_input = ObjectProperty()
     popup = ObjectProperty()
 
+
     def load(self, path, filename):
         with open(os.path.join(path, filename[0])) as stream:
             self.ids.text_input.text = stream.read()
         self.popup.dismiss()
     
+
     def open(self):
-        self.popup = FileChoose(load=self.load)
-        self.popup.open()
+        Tk().withdraw()
+        path = filedialog.askopenfilename()
+        self.file_path = path
+        if path:
+            file = open(path, 'r')
+            contents = file.read()
+            file.close()
+            self.ids.text_input.text = contents
+
+
+    def saveAs(self):
+        Tk().withdraw() # hide tkinter window
+        path = filedialog.asksaveasfilename(defaultextension=".txt",filetypes=[("All Files","*.*"),("Text Documents","*.txt")])
+        if path:
+            file = open(path, 'w')
+            file.write(self.ids.text_input.text)
+    
+    def link(self):
+        Tk().withdraw() # hide tkinter window
+        path = filedialog.askopenfilename()
+        self.file_path = path
+        if path:
+            file = open(path, 'r')
+            contents = file.read()
+            self.ids.text_input.text += '\n\n'
+            self.ids.text_input.text += contents
+
+
+    def new(self):
+        # first clear contents
+        self.ids.text_input.text = ""
+        # functionally the same as saveAs
+        self.saveAs()
+
+    def save(self):
+        # checks whether a previously saved file is currently open
+        global current
+        if current:
+            file = open(current, 'w')
+            file.write(self.ids.text_input.text)
+            file.close()
+            messagebox.showinfo("Save", "File successfully saved.")
+        else:
+            self.saveAs()
+
 
 
 class MyApp(App):
